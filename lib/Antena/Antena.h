b46a879712eead4696555987e4fa2b89ca7bb2f7
armeroitop <armeroitop@gmail.com>
2021-06-20 18:24:32 +0200

    primer intento de refractorización

---
+++ lib/Antena/Antena.h
@@ -0,0 +1,21 @@
+#ifndef __ANTENA_H__
+#define __ANTENA_H__
+
+class Antena
+{
+private:
+    /* data */
+public:
+    Antena(/* args */);
+    ~Antena();
+};
+
+Antena::Antena(/* args */)
+{
+}
+
+Antena::~Antena()
+{
+}
+
+#endif // __ANTENA_H__
---
+++ lib/Pantalla/Pantalla.h
@@ -0,0 +1,24 @@
+#ifndef __PANTALLA_H__
+#define __PANTALLA_H__
+
+
+class Pantalla
+{
+private:
+    /* data */
+public:
+    Pantalla(/* args */);
+    ~Pantalla();
+};
+
+Pantalla::Pantalla(/* args */)
+{
+   
+    
+}
+
+Pantalla::~Pantalla()
+{
+}
+
+#endif // __PANTALLA_H__
